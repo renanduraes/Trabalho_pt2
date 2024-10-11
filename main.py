191130b88cd2d655a1935f631e144233f770f0dc
renanduraes <renanduraes24@gmail.com>
2024-10-11 12:05:21 -0300

    Trabalho part 2 - back end

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Página de contatos
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
+
+# Rota API para buscar os contatos por estado
+@app.route('/api/contatos/<estado>', methods=['GET'])
+def get_contatos(estado):
+    data = { 'AC': {
+                "instituicao": 'Corpo de Bombeiros do Acre',
+                "telefone": '193',
+                "area": 'Combate a incêndios, salvamentos e emergências.',
+                "historia": 'O Corpo de Bombeiros do Acre foi fundado em 1972 e atua em diversas operações de salvamento.'
+            },
+            'AL': {
+                "instituicao": 'Polícia Militar de Alagoas',
+                "telefone": '190',
+                "area": 'Segurança pública e policiamento.',
+                "historia": 'Fundada em 1835, a Polícia Militar de Alagoas é uma das mais antigas instituições de segurança do estado.'
+            },
+            'AM': {
+                "instituicao": 'Defesa Civil do Amazonas',
+                "telefone": '199',
+                "area": 'Prevenção e gestão de desastres naturais.',
+                "historia": 'A Defesa Civil do Amazonas atua em áreas de risco e no controle de emergências ambientais desde 1987.'
+            }}
+    
+    if data:
+        response = data[estado]
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'Contatos não encontrados para este estado.'}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
